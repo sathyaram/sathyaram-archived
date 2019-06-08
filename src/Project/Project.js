6b61308b5dad53762ec7bdcb7223b75be56748e0
Sathya Ram <sathyatheram@gmail.com>
2019-06-08 19:00:52 -0400

    Basic project structure laid out and styled

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -4,15 +4,38 @@ import './Project.scss'
 class Project extends Component {
   render() {
     return (
-      <article style={{ backgroundColor: `${this.props.project.offcolor}` }}>
-        <h3 style={{ color: `${this.props.project.color}` }}>
-          {this.props.project.title}
-        </h3>
-        <h4>{this.props.project.subtitle}</h4>
-        <ul>{this.props.project.tags}</ul>
+      <article >
+        <div className="heading">
+          <h3 className="title" style={{ color: `${this.props.project.color}` }}>
+            {this.props.project.title}
+          </h3>
+          <h4 className="subtitle">{this.props.project.subtitle}</h4>
+        </div>
         <ul>{console.log(this.props.project.tags)}</ul>
-        <a href={this.props.project.link}>View Website</a>
-        <a href={this.props.project.designs}>View Designs</a>
+        <div className="text">
+        <p className="description">{this.props.project.description}</p>
+
+          <ul className="tags">
+            <li>Drupal 8</li>
+            <li>Node.js</li>
+            <li>React.js</li>
+            <li>Express.js</li>
+            <li>AWS</li>
+          </ul>
+          <div className="links">
+            <a className="link" href={this.props.project.link}><i class="fas fa-globe-americas"></i>{this.props.project.link}</a>
+            <a className="designs" href={this.props.project.designs}><i class="fab fa-dribbble" aria-hidden="true"></i>View Designs</a>
+          </div>
+        </div>
+        <div className="track" style={{ backgroundColor: `${this.props.project.offcolor}` }}>
+          <div className="grid">
+            <img src="https://picsum.photos/500/500" />
+            <img src="https://picsum.photos/501/501" />
+            <img src="https://picsum.photos/502/502" />
+            <img src="https://picsum.photos/502/501" />
+            <img src="https://picsum.photos/503/502" /> 
+          </div>
+        </div>
 {/*         
         <ul>
           {this.props.project.tags.map(tag => <li>{tag.value}</li>)}
