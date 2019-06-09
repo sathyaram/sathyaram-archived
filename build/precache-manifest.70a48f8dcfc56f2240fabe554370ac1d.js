20e5526d19d884ea7fff0dcb7c3d31228feaa685
Sathya Ram <sathyatheram@gmail.com>
2019-06-08 22:19:48 -0400

    added build

---
+++ build/precache-manifest.70a48f8dcfc56f2240fabe554370ac1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4576168be97e29c2bbe7f0065594f83d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "905907f302327c31c28f",
+    "url": "/static/css/main.3e7c7951.chunk.css"
+  },
+  {
+    "revision": "6caf316fcea33408934b",
+    "url": "/static/js/2.5bceac50.chunk.js"
+  },
+  {
+    "revision": "905907f302327c31c28f",
+    "url": "/static/js/main.7d3e8e11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "071e62f2f802ddac0ede2273ef0abe19",
+    "url": "/static/media/ram-logo-white.071e62f2.svg"
+  }
+]);
