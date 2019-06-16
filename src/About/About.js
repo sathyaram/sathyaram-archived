4581f3ab131098cbceb620bb60cacc4a2491b2b6
Sathya Ram <sathyatheram@gmail.com>
2019-06-16 15:11:36 -0400

    Page clean up

--- src/About/About.js
+++ src/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './About.scss';
+import me from './../me.jpg';
 
 class About extends Component {
   render() {
@@ -11,16 +12,24 @@ class About extends Component {
           <br></br>
 
           I'm Sathya Ram,
-          an Indian dude who loves to make creative shit with polish.
+          <br></br>
+          {/* <img src={me} alt="sathya ram" ></img> */}
+          a dude who loves to make creative ...anything! Must be polished, though*
           <br></br>
           <br></br>
           I live in Alexandria, Virginia and work full time
           as a Front-End Developer for a wholesome agency called <a target="_blank" rel="noopener noreferrer" href="https://forumone.com">Forum One</a>. 
           <br></br>
           <br></br>
+          
+          {/* As far as development, I'm really into animated interations, SVGs, Javascript Animation, Gatsby.js, and trying to make CSS art lately. 
+          <br></br>
+          <br></br> */}
           In my non-existant free time, I gorge on video games, <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/sathya.ram">shoot some neat photos</a>, dream up short film ideas,
-          think about going to the gym, waste my life on reddit, and watch a metric fudgeton of television and movies.
-
+          think about going to the gym, draw poorly, waste my life redditing, and watch a metric fudgeton of television and movies.
+          <br></br>
+          <br></br>
+          Oh, since I'm obsessed with fonts, you absolutely must know: this site uses <a target="_blank" rel="noopener noreferrer" href="https://freetypography.com/2015/08/09/free-font-aquatico/">Aquatico</a> and <a target="_blank" rel="noopener noreferrer" href="https://fonts.google.com/specimen/Barlow" >Barlow</a>.
         </div>
 
       </main>
