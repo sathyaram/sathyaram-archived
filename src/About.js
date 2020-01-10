3649fe7a35b1fe08a3463260149c6da788c7a5d2
sathyaram <sram@forumone.com>
2020-01-10 17:20:54 -0500

    slight cleanup

--- src/About.js
+++ src/About.js
@@ -9,17 +9,19 @@ class About extends Component {
           <h6><div>Stirrin' up trouble since 1994</div></h6>
           <img className="appear" alt="Peter" src="/images/Untitled-3.png"></img>
           <div className="about">
-            Hey hey! 
-            I'm <strong>Sathya Ram</strong>, 
-            <br></br> 
-            an all encompassing nerd, CSS Junkie, and avid conversationalist.
-            <br></br> 
-            <br></br> 
+            Hey hey!
+            I'm <strong>Sathya Ram</strong>,
+            <br></br>
+            a passionate nerd, CSS Junkie, and avid conversationalist.
+            <br></br>
+            <br></br>
+            I work at a mission-driven agency called <a target="_blank" rel="noopener noreferrer" href="https://forumone.com">Forum One</a>, doing front-end work for non-profit/government clients.
+            <br></br>
+            <br></br>
             I love all things film/TV. I'm a big <a target="_blank" rel="noopener noreferrer" href="https://worldofwarcraft.com/en-us/character/us/lightbringer/sathex">World of Warcraft</a> player, and amateur voice actor.
             <br></br>
-            <br></br> 
-            I work at a mission-driven agency called <a target="_blank" rel="noopener noreferrer" href="https://forumone.com">Forum One</a>, doing frontend work for non-profit/government clients.
-     
+            <br></br>
+            I post things on <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/sathya.ram">my Instagram</a>, so follow me if you're bored.
           </div>
         </div>
       </div>
