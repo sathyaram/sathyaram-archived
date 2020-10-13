8dcbdcfb1fe53c52af6645f85bf5761d5ef06268
Sathya Ram <sathyaram@Sathyas-MBP.hsd1.va.comcast.net>
2020-10-13 09:01:59 -0400

    7.0 - second  push

---
+++ build/precache-manifest.8a43f6dcf1ccc094bc759359a4ce40f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38ff752211096a831a3d120655fc560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c769de80fa72ba4f69",
+    "url": "/static/css/main.ae10c07d.chunk.css"
+  },
+  {
+    "revision": "19571c062d48d2eb5d3b",
+    "url": "/static/js/2.c518197a.chunk.js"
+  },
+  {
+    "revision": "989ac8b04042782519a29910086adb47",
+    "url": "/static/js/2.c518197a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c769de80fa72ba4f69",
+    "url": "/static/js/main.4825fe3a.chunk.js"
+  },
+  {
+    "revision": "710272b27423f6fc06ec",
+    "url": "/static/js/runtime-main.75cf77cb.js"
+  }
+]);
