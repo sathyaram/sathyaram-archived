79f57c6e0d559c0d829551aed0c39e20cc73f5c2
Sathya Ram <sathyaram@Sathyas-MacBook-Pro.local>
2021-02-27 21:28:36 -0500

    swiper thumbnail pretty much working

--- src/pages/portraits.js
+++ src/pages/portraits.js
@@ -1,43 +1,50 @@
-import React from "react"
+import React, {useEffect, useRef, useState} from "react"
 import Swiper from "react-id-swiper";
 import Menu from "../components/menu"
 import Sky from "../components/sky"
 import SEO from "../components/seo"
 
-
 const Portraits = () => {
-  var params = {
-    grabCursor: true,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    freeMode: true,
-    loop: true,
-  };
-
-  
-  
-  var params1 = {
+  var galleryParams = {
     spaceBetween: 10,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     thumbs: {
-      swiper: params2
+      swiper: thumbParams
     }
   }
 
-  var params2 = {
+  var thumbParams = {
+    // spaceBetween: 10,
+    // slidesPerView: 4,
+    // freeMode: true,
+    // watchSlidesVisibility: true,
+    // watchSlidesProgress: true,
+    // slideToClickedSlide: true
+
     spaceBetween: 10,
+      // centeredSlides: true,
     slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
+
+      // slidesPerView: 'auto',
+      touchRatio: 0.2,
+      slideToClickedSlide: true
   }
+
+  const gallerySwiperRef = useRef(null);
+  const thumbnailSwiperRef = useRef(null);
+
+  useEffect(() => {
+    const gallerySwiper = gallerySwiperRef.current.swiper;
+    const thumbnailSwiper = thumbnailSwiperRef.current.swiper;
+    if (gallerySwiper.controller && thumbnailSwiper.controller
+    ) {
+      gallerySwiper.controller.control = thumbnailSwiper;
+      thumbnailSwiper.controller.control = gallerySwiper;
+    }
+  }, []);
   
   
 
@@ -50,32 +57,57 @@ const Portraits = () => {
         <h1>Portraits</h1>
       </header>
       <div className="photography-content">
-        <Swiper {...params1}>
+        <Swiper {...galleryParams} ref={gallerySwiperRef}>
+        <img
+        alt="me"
+        src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
+        />
+
+        <img
+          alt="me"
+          src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
+        />
+
+        <img
+          alt="me"
+          src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
+        />
+
+        <img
+          alt="me"
+          src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
+        />
+
+        <img
+          alt="me"
+          src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
+        />
+        </Swiper>
+        <Swiper {...thumbParams} ref={thumbnailSwiperRef}>
+          <img
+          alt="me"
+          src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
+          />
+
           <img
             alt="me"
             src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
           />
-  
+
           <img
             alt="me"
             src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
           />
-  
+
           <img
             alt="me"
             src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
           />
-  
+
           <img
             alt="me"
             src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
           />
-  
-          <img
-            alt="me"
-            src="https://uploads.codesandbox.io/uploads/user/e07fd652-6bd5-4bc0-a542-41d98d7af4e0/gEp3-DSC00985.jpg"
-          />
-  
         </Swiper>
       </div>
     </div>
